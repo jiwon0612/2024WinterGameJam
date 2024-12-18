0288178b90f4e0605b7c83cf540bfd586f88d751
박준서 <pppg12jintpt@gmail.com>
2024-12-18 16:38:45 +0900

    remove:bezier gizmo

--- Assets/00.Work/Junser/0.Script/Util/Util.cs
+++ Assets/00.Work/Junser/0.Script/Util/Util.cs
@@ -17,26 +17,7 @@ public class Util
 
         return result;
     }
-    void Update()
-    {
-        
-
-        int segmentCount = 20;
-        Vector3 previousPoint = Vector3.zero;
-
-        for (int i = 1; i <= segmentCount; i++)
-        {
-            float t = i / (float)segmentCount;
-            Vector3 pointOnCurve = CubicBezierCurve(new Vector3(0, 0, 0),
-            new Vector3(0, 15f, 5f),
-            new Vector3(0, 1, 7.5f),
-            new Vector3(0, 0, 30), t);
-
-            // Draw line between the previous point and the current point
-            Debug.DrawLine(previousPoint, pointOnCurve, Color.red);
-            previousPoint = pointOnCurve;
-        }
-    }
+    
     public static float InverseVectorLerp(Vector3 a, Vector3 b, Vector3 value)
     {
         Vector3 ab = b - a;
