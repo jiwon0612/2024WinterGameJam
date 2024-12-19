aded3c23aa4817bbe7741c1d6d8b2eb68a18b218
박준서 <pppg12jintpt@gmail.com>
2024-12-20 02:20:30 +0900

    feat:hardcoding runoutcheck

--- Assets/00.Work/Junser/0.Script/FSM/BewallTheEagleRunningOutState.cs
+++ Assets/00.Work/Junser/0.Script/FSM/BewallTheEagleRunningOutState.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 public class BewallTheEagleRunningOutState : EntityState
 {
     private BewallTheEagle _boss;
+    private Collider _collider;
     public BewallTheEagleRunningOutState(Entity entity, int i) : base(entity, i)
     {
         _boss = entity as BewallTheEagle;
@@ -15,10 +16,17 @@ public class BewallTheEagleRunningOutState : EntityState
     {
         base.Enter();
         _boss.OnRunOut?.Invoke();
+        _collider = _boss.GetComponent<Collider>();
+        _collider.isTrigger = true;
         Sequence sequence = DOTween.Sequence();
         sequence.Append(_boss.transform.DOMove(_boss._attackComponent.player.position - _boss.transform.position, _boss.runoutTime)
-            .SetEase(Ease.InQuint).SetLoops(2, LoopType.Yoyo))
-            .AppendCallback(()=>_boss.ChangeState(StateName.Idle));
+            .SetEase(Ease.InQuint)
+            .SetLoops(2, LoopType.Yoyo))
+            .AppendCallback(()=>
+            { 
+                _boss.ChangeState(StateName.Idle);
+                _collider.isTrigger = false;
+            });
         sequence.Play();
     }
 }
